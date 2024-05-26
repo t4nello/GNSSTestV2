5208c1b8ba2de3b7f985e96184056ec64b1dab20
t4nello <81033764+t4nello@users.noreply.github.com>
2024-05-26 10:47:35 +0200

    Update router.py

--- GNSS_Serializer/router.py
+++ GNSS_Serializer/router.py
@@ -5,17 +5,37 @@ class Router:
         self.postgres_manager = postgres_manager
 
     def configure_routes(self, app):
-        @app.route('/sessions', methods=['GET'])
+        @app.route('/api/sessions', methods=['GET'])
         def get_sessions():
             unique_session_ids = self.postgres_manager.get_unique_session_ids()
             return jsonify({"sessions": unique_session_ids})
 
-        @app.route('/devices', methods=['GET'])
+        @app.route('/api/devices', methods=['GET'])
         def get_devices():
             unique_devices = self.postgres_manager.get_unique_devices()
             return jsonify({"devices": unique_devices})
-        
-        @app.route('/latitude/<selected_session>', methods=['GET'])
-        def get_latitude(selected_session):
-            latitude = self.postgres_manager.get_selected_latitude(selected_session)
-            return jsonify({"latitude": latitude})
+
+        @app.route('/api/<device>/<field>/<sessionid>', methods=['GET'])
+        def get_data(device, field, sessionid):
+            valid_fields = ["latitude", "longitude", "position", "speed", "satellites", "altitude"]
+            if field not in valid_fields:
+                    return jsonify({"error": "Invalid field"}), 400
+            if device == "all":
+                if field == "position":
+                    data = self.postgres_manager.get_position_for_session(sessionid)
+                else:
+                    data = self.postgres_manager.get_measurand_for_device(field, sessionid)
+            else:
+                if field == "position":
+                    data = self.postgres_manager.get_position_for_device(device, sessionid)
+                else: 
+                    data = self.postgres_manager.get_measurand_for_session(device, field ,sessionid)
+            if data is not None:
+                return jsonify(data)
+            else:
+                return jsonify({"error": "Invalid request or missing data"}), 400  
+
+        @app.route('/api/<session>/devices', methods=['GET'])
+        def get_devices_per_session(session):
+            unique_devices = self.postgres_manager.get_unique_devices_per_session(session)
+            return jsonify({"devices": unique_devices})
