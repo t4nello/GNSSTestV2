405055588f5d0db63be223419828dc9e6b3c6286
t4nello <81033764+t4nello@users.noreply.github.com>
2024-05-26 10:48:22 +0200

    Update main.py

--- GNSS_Serializer/main.py
+++ GNSS_Serializer/main.py
@@ -1,15 +1,16 @@
+import threading
 import paho.mqtt.client as mqtt
 from flask import Flask
-from GNSS_Serializer.DatabaseManager import PostgresDBManager
+from DatabaseManager import PostgresDBManager
 from gps_metric_handler import GpsMetricHandler
 from config_manager import ConfigManager
 from session_manager import SessionManager
 from algorithm import Algorithm
 from router import Router
-def main():
+
+def mqtt_loop():
+    mqtt_handler = mqtt.Client("gnssSerializer")
     config_manager = ConfigManager("config.json")
-    id = "gnssSerializer"
-    mqtt_handler = mqtt.Client(id)
     session_manager = SessionManager(mqtt_handler, config_manager)
     algorithm_instance = Algorithm(mqtt_handler, session_manager)
     gps_handler = GpsMetricHandler(mqtt_handler, config_manager, session_manager, algorithm_instance)
@@ -17,14 +18,22 @@ def main():
     mqtt_handler.on_connect = gps_handler.on_connect
     mqtt_handler.on_message = gps_handler.on_message
 
-    mqtt_handler.connect("192.168.0.213", 1883)
+    mqtt_handler.connect("localhost", 1883)
+    mqtt_handler.loop_forever()
 
+def flask_run():
     router = Router(PostgresDBManager("host=localhost user=postgres password=admin123 dbname=tsdb"))
     app = Flask(__name__)
     router.configure_routes(app)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    app.config['JSON_SORT_KEYS'] = False
 
-    mqtt_handler.loop_forever()
+def main():
+    mqtt_thread = threading.Thread(target=mqtt_loop)
+    flask_thread = threading.Thread(target=flask_run)
+
+    mqtt_thread.start()
+    flask_thread.start()
 
 if __name__ == "__main__":
     main()
